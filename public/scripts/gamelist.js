1270a76aa1b98ea2ea88958d663ada784484eb96
Vaughan Milliman <vaughanm1@protonmail.com>
2022-03-12 01:19:26 -0600

    initial commit of new branch. hot corn 1.0.0 has been released

---
+++ public/scripts/gamelist.js
@@ -0,0 +1,27 @@
+var gamelist = {
+	"shell": "https://algebra.best",
+	"1v1": "https://1v1.lol",
+	"merczone": "https://merc.zone",
+	"zombs": "https://zombsroyale.io",
+	"cookie": "https://hotcorn-gamecdn.web.app/cookie/",
+	"curvefever": "https://curvefever.pro",
+	"ev": "https://ev.io",
+	"minecraft": "minecraft.html",
+	"repuls": "https://repuls.io",
+	"sans": "https://jcw87.github.io/c2-sans-fight/",
+	"smashkarts": "https://smashkarts.io",
+};
+
+var gamenames = {
+	"shell": "Shell Shockers",
+	"1v1": "1v1.lol",
+	"merczone": "Merc Zone",
+	"zombs": "Zombs Royale",
+	"cookie": "Cookie Clicker",
+	"curvefever": "Curve Fever",
+	"ev": "ev.io",
+	"minecraft": "WebMC (Minecraft)",
+	"repuls": "Repuls",
+	"sans": "Sans Bossfight",
+	"smashkarts": "Smash Karts",
+};
