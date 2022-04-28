4643dd19b79580f30449ee7577adbe22bec39695
Vaughan Milliman <vaughanm1@protonmail.com>
2022-04-28 17:10:53 -0500

    reedited logo back and fixed up some stuff in commit pusher

--- push-deploy.sh
+++ push-deploy.sh
@@ -2,15 +2,19 @@
 lastcommit="$(git log -1 --pretty=%B)"
 
 for file in $(find .); do
-	sed -i -re 's/(((src)|(href))=\"\/)/\1corn\//g' $file
-	git add $file
+	if [[ "$file" == *".html"* ]]; then
+		sed -i -re 's/(((src)|(href))=\"\/)/\1corn\//g' $file
+		git add $file
+	fi
 done
 git commit -m "file fix for subtree"
 git subtree push --prefix public origin gh-pages
 
 for file in $(find .); do 
-	sed -i -re 's/(((src)|(href))=\")\/corn/\1/g' $file
-	git add $file
+	if [[ "$file" == *".html"* ]]; then
+		sed -i -re 's/(((src)|(href))=\")\/corn/\1/g' $file
+		git add $file
+	fi
 done
 
 git commit -m "$lastcommit"
