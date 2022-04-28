49927e3082333cafcd572ecfd61d753cafb334f8
Vaughan Milliman <vaughanm1@protonmail.com>
2022-04-28 17:16:01 -0500

    this is hard

--- push-deploy.sh
+++ push-deploy.sh
@@ -2,7 +2,8 @@
 lastcommit="$(git log -1 --pretty=%B)"
 
 for file in $(find .); do
-	if [[ "$file" = "*.html*" ]]; then
+	if [[ "$file" = "*.html" ]]; then
+		echo "changing $file"
 		sed -i -re 's/(((src)|(href))=\"\/)/\1corn\//g' $file
 		git add $file
 	fi
@@ -11,7 +12,8 @@ git commit -m "file fix for subtree"
 git subtree push --prefix public origin gh-pages
 
 for file in $(find .); do 
-	if [[ "$file" = "*.html*" ]]; then
+	if [[ "$file" = "*.html" ]]; then
+		echo "reverting $file"
 		sed -i -re 's/(((src)|(href))=\")\/corn/\1/g' $file
 		git add $file
 	fi
