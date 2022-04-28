df4e8df326990b008a216dbaddc53db3f00cf334
Vaughan Milliman <vaughanm1@protonmail.com>
2022-04-28 17:19:29 -0500

    changed uploader script so it works now yay

--- push-deploy.sh
+++ push-deploy.sh
@@ -2,7 +2,7 @@
 lastcommit="$(git log -1 --pretty=%B)"
 
 for file in $(find .); do
-	if [[ "$file" = "*.html" ]]; then
+	if [[ "$file" == *".html" ]]; then
 		echo "changing $file"
 		sed -i -re 's/(((src)|(href))=\"\/)/\1corn\//g' $file
 		git add $file
@@ -12,13 +12,12 @@ git commit -m "file fix for subtree"
 git subtree push --prefix public origin gh-pages
 
 for file in $(find .); do 
-	if [[ "$file" = "*.html" ]]; then
+	if [[ "$file" == *".html" ]]; then
 		echo "reverting $file"
 		sed -i -re 's/(((src)|(href))=\")\/corn/\1/g' $file
 		git add $file
 	fi
 done
-
 git commit -m "$lastcommit"
 git push origin master
 firebase deploy
