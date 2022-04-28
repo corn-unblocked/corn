b2ef61ad4749f3bf1d6141431d46a5924c8229b0
Vaughan Milliman <vaughanm1@protonmail.com>
2022-04-28 17:13:40 -0500

    fixed push deploy again

--- push-deploy.sh
+++ push-deploy.sh
@@ -2,7 +2,7 @@
 lastcommit="$(git log -1 --pretty=%B)"
 
 for file in $(find .); do
-	if [[ "$file" == *".html"* ]]; then
+	if [[ "$file" = "*.html*" ]]; then
 		sed -i -re 's/(((src)|(href))=\"\/)/\1corn\//g' $file
 		git add $file
 	fi
@@ -11,7 +11,7 @@ git commit -m "file fix for subtree"
 git subtree push --prefix public origin gh-pages
 
 for file in $(find .); do 
-	if [[ "$file" == *".html"* ]]; then
+	if [[ "$file" = "*.html*" ]]; then
 		sed -i -re 's/(((src)|(href))=\")\/corn/\1/g' $file
 		git add $file
 	fi
