7b87e2a8602fb5050389de572a5f062bf94d2252
Vaughan Milliman <vaughanm1@protonmail.com>
2022-07-20 16:14:52 -0500

    added firebase in npm

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDUCu1-mV98QJfo8VAyO-D2OB9SthwJ65Y",
+
+  authDomain: "hot-corn-5a8ba.firebaseapp.com",
+
+  projectId: "hot-corn-5a8ba",
+
+  storageBucket: "hot-corn-5a8ba.appspot.com",
+
+  messagingSenderId: "945726798663",
+
+  appId: "1:945726798663:web:632078f55968f1d1fd6a62"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
