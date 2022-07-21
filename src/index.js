ffca8f2c22d2114f520b56181104bcd89ca5287a
Vaughan Milliman <vaughanm1@protonmail.com>
2022-07-20 23:26:55 -0500

    Added theme editor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getDatabase } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
