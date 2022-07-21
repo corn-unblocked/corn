c0f86612545e1d4b3b0a81afe374b171c21d4c82
Vaughan Milliman <vaughanm1@protonmail.com>
2022-07-20 21:59:30 -0500

    set up node more

--- build.sh
+++ build.sh
@@ -9,12 +9,16 @@ fi
 case $1 in
 	"build")
 		mkdir public
+		mkdir dist
+		npm run build
 		for file in "404.html about css devtools games img index.html license.txt other pr0xy scripts"; do
 			cp -r $file public/
 		done
+		mv dist/bundle.js public/scripts/
 		;;
 	"clean")
 		rm -r public
+		rmdir dist
 		;;
 	*)
 		echo "Invalid command specified, please specify either build or clean"
--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
 // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
-
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-
-  apiKey: "AIzaSyDUCu1-mV98QJfo8VAyO-D2OB9SthwJ65Y",
-
-  authDomain: "hot-corn-5a8ba.firebaseapp.com",
-
-  projectId: "hot-corn-5a8ba",
-
-  storageBucket: "hot-corn-5a8ba.appspot.com",
-
-  messagingSenderId: "945726798663",
-
-  appId: "1:945726798663:web:632078f55968f1d1fd6a62"
-
+	apiKey: "AIzaSyDUCu1-mV98QJfo8VAyO-D2OB9SthwJ65Y",
+	authDomain: "hot-corn-5a8ba.firebaseapp.com",
+	projectId: "hot-corn-5a8ba",
+	storageBucket: "hot-corn-5a8ba.appspot.com",
+	messagingSenderId: "945726798663",
+	appId: "1:945726798663:web:632078f55968f1d1fd6a62",
+	databaseURL: "https://hot-corn-5a8ba-default-rtdb.firebaseio.com"
 };
 
-
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
