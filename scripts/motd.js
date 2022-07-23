5ba5f097ed41e508093257eaba5fe1a86568aa4c
Vaughan Milliman <vaughanm1@protonmail.com>
2022-07-23 11:49:19 -0500

    added motds

---
+++ scripts/motd.js
@@ -0,0 +1,12 @@
+var motds = [
+    "Hot Corn Games - Not what you think",
+    "You should play Terraria",
+    "Go do your schoolwork",
+    "The proxy is down... again",
+    "Try our new theme feature!",
+    "Ademay ybay Aughanvay Illimanmay",
+    "Try to actually win this time",
+    "Life is long, games are forever",
+    "ily anvitha",
+    "Cry about it"
+];
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getDatabase } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const auth = getAuth();
