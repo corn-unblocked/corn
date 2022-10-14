ffcc3b497bf0309de72c2aeca3599cd380c6bbcc
Vaughan Milliman <vaughanm1@protonmail.com>
2022-10-13 19:01:07 -0500

    migrating away from heroku

--- scripts/devtools.js
+++ scripts/devtools.js
@@ -1,11 +1,11 @@
 var gamelist = {
 	"wandbox": "https://wandbox.org",
 	"jsitor": "https://jsitor.com",
-	"sshwifty": "https://sshwifty.herokuapp.com",
+	"sshwifty": "https://sshwifty-demo.nirui.org",
 };
 
 var gamenames = {
 	"wandbox": "Wandbox",
 	"jsitor": "JSitor",
-	"sshwifty": "Sshwifty",
+	"sshwifty": "SSHwifty",
 };
